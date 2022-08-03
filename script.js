fb7d0d93cee1c59ac5d1fc65685ec58ed666e991
Thiesa <thiesa.c@gmail.com>
2022-08-03 18:00:40 -0400

    função sort corrigida

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 let numeros = [];
 
 function inserirNumeros() {
-    let insereNumero = document.getElementById('inserir').value;
+  let insereNumero = document.getElementById("inserir").value;
 
-    if (insereNumero == "") {
-        alert("Insira números");
-    } else {
-        numeros.push(insereNumero);
-    }
-    document.getElementById('inserir').value = "";
-    document.getElementById('numeros-adicionados').innerText = numeros.join(" | ");
+  if (insereNumero == "") {
+    alert("Insira números");
+  } else {
+    numeros.push(insereNumero);
+  }
+  document.getElementById("inserir").value = "";
+  document.getElementById("numeros-adicionados").innerText =
+    numeros.join(" | ");
 }
 
 function ordenarNumeros() {
-    if (numeros == "") {
-        alert("Insira números")
-    } else {
-        let numerosEspacados = numeros.sort(); 
-        document.getElementById('numeros-ordenados').innerText = numerosEspacados.join(" | ");
-    }
+  if (numeros == "") {
+    alert("Insira números");
+  } else {
+    let numerosEspacados = numeros.sort(function (a, b) {
+      return a - b;
+    });
+    document.getElementById("numeros-ordenados").innerText =
+      numerosEspacados.join(" | ");
+  }
 }
